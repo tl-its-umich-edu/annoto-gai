d01dd4253149ed84dddbef4ee65e001315b36e6b
Kapotaksha <takposha@umich.edu>
2024-05-17 12:09:32 -0400

    Addresses the Topic Extraction component of #9, with save functionality and improved ReadMe

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # annoto-gai
 This is the Github Project to Annoto GAI work.
 
-Install required additonal libraries using requirements.txt.
+Initial setup: 
+> Install required additonal libraries using `requirements.txt`.  
+> Use the `.env.sample` file in the `config` folder to create an `.env.` file to use with the needed credentials to use UM's OpenAI API.  
+> For each video, create a folder in the `Captions` Folder, containing the corresponding .srt file within it. The first .srt file found in the subfolder will be used.  
 
-For each video, create a folder in the Captions Folder, containing the corresponding .srt file within it. The first .srt file found in the subfolder will be used. 
-The captionsProcessor notebook reads captions in the .srt file format provided within the subfolders of the Captions folder.
+You can access videos from [MiVideo](https://www.mivideo.it.umich.edu/). It is recommended to use a video that is over 15 minutes long, and it must have .srt filetype captions available for it. Being familiar with the video contents can help in determining the utility and corectness of the topics extracted.
 
-The getCombinedTranscripts function when run on a single video file name will return a dataframe of the segmented transcript, where each line has an approximate duration, 30s in this case. This will be later used for topic extraction and segmentation.
+The `captionsProcessor.ipynb` notebook reads captions in the .srt file format provided within the subfolders of the `Captions` folder. Read the notes in the notebook as well for further information about adjusting certain parameters in the `config` Class for managing the choice of Representation model used, and the usage of KeyBERT.
+
+The `getCombinedTranscripts` function when run on a single video file name will return a dataframe of the segmented transcript provided within the subfolders of the `Captions` folder, where each line has an approximate duration, 30s in this case. This will be used for topic extraction and segmentation.
--- helper.py
+++ helper.py
@@ -6,9 +6,21 @@ import pickle
 from datetime import datetime
 import pandas as pd
 
-from dotenv import load_dotenv
+from openai import AzureOpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain_openai import AzureChatOpenAI as langchainAzureOpenAI
 
+from keybert import KeyBERT
+from sklearn.feature_extraction.text import CountVectorizer
+
+from bertopic import BERTopic
+from bertopic.representation import KeyBERTInspired, OpenAI, LangChain
+
+from dotenv import load_dotenv
 load_dotenv()
+
+import numpy as np
+np.seterr(divide="ignore")
 
 
 def processSrtFile(srtFile):
@@ -51,13 +63,13 @@ def processSrtFile(srtFile):
     return pd.DataFrame(transcript)
 
 
-def lineCombiner(transcript, windowSize=20):
+def lineCombiner(transcript, windowSize=30):
     """
     Combines consecutive lines in a transcript within a specified time window.
 
     Args:
         transcript (pandas.DataFrame): The transcript data containing columns "Start" and "Line".
-        windowSize (int, optional): The time window size in seconds. Defaults to 20.
+        windowSize (int, optional): The time window size in seconds. Defaults to 30.
 
     Returns:
         pandas.DataFrame: A DataFrame containing the combined lines with their start and end times.
@@ -124,6 +136,214 @@ def getCombinedTranscripts(
     return combinedTranscripts
 
 
+def retrieveTopics(
+    videoToUse, transcriptToUse, config, overwrite=False, saveNameAppend=""
+):
+    """
+    Retrieves or generates topic model and data.
+
+    Args:
+        videoToUse (str): Path to the video file.
+        transcriptToUse (str): Path to the transcript file.
+        config (dict): Configuration settings.
+        overwrite (bool, optional): Whether to overwrite existing data. Defaults to False.
+        saveNameAppend (str, optional): String to append to the save name. Defaults to "".
+
+    Returns:
+        tuple: A tuple containing the topics over time and the topic model.
+    """
+
+    if (
+        (topicModel := dataLoader(config, "topicModel", videoToUse, saveNameAppend))
+        is not False
+        and (
+            topicsOverTime := dataLoader(
+                config, "topicsOverTime", videoToUse, saveNameAppend
+            )
+        )
+        is not False
+        and not overwrite
+    ):
+        logging.info("Topic Model and Data loaded from saved files.")
+        return topicsOverTime, topicModel
+
+    logging.info("Generating & saving Topic Model and Data.")
+    topicsOverTime, topicModel = getTopicsOverTime(transcriptToUse, config=config)
+    dataSaver(topicModel, config, "topicModel", videoToUse, saveNameAppend)
+    dataSaver(
+        topicsOverTime,
+        config,
+        "topicsOverTime",
+        videoToUse,
+        saveNameAppend,
+    )
+
+    return topicsOverTime, topicModel
+
+
+def getVectorizer(docs):
+    """
+    Returns a CountVectorizer model initialized with a vocabulary extracted from the given documents.
+
+    Parameters:
+    - docs (list): A list of documents to extract keywords from.
+
+    Returns:
+    - vectorizer_model (CountVectorizer): A CountVectorizer model initialized with the extracted vocabulary.
+    """
+    kw_model = KeyBERT()
+    keywords = kw_model.extract_keywords(docs, keyphrase_ngram_range=(1, 2))
+
+    vocabulary = [k[0] for keyword in keywords for k in keyword]
+    vocabulary = list(set(vocabulary))
+
+    vectorizer_model = CountVectorizer(vocabulary=vocabulary)
+    return vectorizer_model
+
+
+def getBinCount(combinedTranscript, windowSize=120):
+    """
+    Calculates the number of bins based on the combined transcript and window size.
+
+    Parameters:
+    combinedTranscript (DataFrame): The combined transcript containing the start and end times.
+    windowSize (int): The size of each window in seconds. Default is 120.
+
+    Returns:
+    int: The number of bins calculated based on the video duration and window size.
+    """
+    videoDuration = (
+        combinedTranscript["End"].iloc[-1] - combinedTranscript["Start"].iloc[0]
+    )
+    binCount = int(videoDuration.total_seconds() // windowSize)
+    return binCount
+
+
+def getTopicsOverTime(combinedTranscript, config):
+    """
+    Get topics over time from a combined transcript.
+
+    Args:
+        combinedTranscript (DataFrame): The combined transcript containing the lines and timestamps.
+        config (Config): The configuration object specifying the representation model type and other settings.
+
+    Returns:
+        tuple: A tuple containing the topics over time and the topic model.
+
+    Raises:
+        None
+
+    """
+
+    docs = combinedTranscript["Combined Lines"].tolist()
+    timestamps = combinedTranscript["Start"].tolist()
+
+    if config.representationModelType == "simple" or config is None:
+        representation_model = KeyBERTInspired()
+
+    if config.representationModelType == "openai":
+        import tiktoken
+
+        OpenAIChatBot = OpenAIBot(config)
+        tokenizer = tiktoken.encoding_for_model(OpenAIChatBot.model)
+        representation_model = OpenAI(
+            OpenAIChatBot.client,
+            model=OpenAIChatBot.model,
+            delay_in_seconds=2,
+            chat=True,
+            nr_docs=8,
+            doc_length=None,
+            tokenizer=tokenizer,
+        )
+    if config.representationModelType == "langchain":
+        LangChainQABot = LangChainBot(config)
+        chain = LangChainQABot.chain
+        prompt = "Give a single label that is only a few words long to summarize what these documents are about"
+        representation_model = LangChain(chain, prompt=prompt)
+
+    if config.useKeyBERT:
+        topicModel = BERTopic(
+            representation_model=representation_model,
+            vectorizer_model=getVectorizer(docs),
+        )
+    else:
+        topicModel = BERTopic(representation_model=representation_model)
+
+    topics, probs = topicModel.fit_transform(docs)
+
+    # hierarchical_topics = topic_model.hierarchical_topics(docs)
+    # topic_model.visualize_hierarchy(hierarchical_topics=hierarchical_topics)
+
+    binCount = getBinCount(combinedTranscript, windowSize=120)
+    topicsOverTime = topicModel.topics_over_time(docs, timestamps, nr_bins=binCount)
+
+    # topicModel.visualize_topics_over_time(topicsOverTime)
+
+    return topicsOverTime, topicModel
+
+
+def dataSaver(data, config, dataType, videoToUse, saveNameAppend=""):
+    """
+    Save the data based on the specified configuration.
+
+    Args:
+        data: The data to be saved.
+        config: The configuration object.
+        dataType: The type of data being saved.
+        videoToUse: The video identifier.
+        saveNameAppend: An optional string to append to the save name.
+
+    Returns:
+        The path where the data is saved.
+    """
+    if config.useKeyBERT:
+        saveNameAppend = f"_KeyBERT{saveNameAppend}"
+    saveName = f"{videoToUse}_{config.representationModelType}{saveNameAppend}"
+    savePath = os.path.join(config.saveFolder, dataType, saveName)
+
+    if dataType == "topicModel":
+        data.save(
+            savePath,
+            serialization="safetensors",
+            save_ctfidf=True,
+            save_embedding_model="sentence-transformers/all-MiniLM-L6-v2",
+        )
+    if dataType == "topicsOverTime":
+        pickle.dump(data, open(savePath + ".p", "wb"))
+
+    return savePath
+
+
+def dataLoader(config, dataType, videoToUse, saveNameAppend=""):
+    """
+    Load data based on the specified configuration, data type, video to use, and save name appendix.
+
+    Parameters:
+    - config: The configuration object.
+    - dataType: The type of data to load.
+    - videoToUse: The video to use for loading the data.
+    - saveNameAppend: An optional appendix to add to the save name.
+
+    Returns:
+    - The loaded data if it exists, otherwise False.
+    """
+    if config.useKeyBERT:
+        saveNameAppend = f"_KeyBERT{saveNameAppend}"
+    if dataType == "topicsOverTime":
+        saveNameAppend = f"{saveNameAppend}.p"
+
+    saveName = f"{videoToUse}_{config.representationModelType}{saveNameAppend}"
+    savePath = os.path.join(config.saveFolder, dataType, saveName)
+
+    if os.path.exists(savePath):
+        if dataType == "topicModel":
+            return BERTopic.load(savePath)
+        if dataType == "topicsOverTime":
+            return pickle.load(open(savePath, "rb"))
+
+    return False
+
+
 class Config:
     """
     Configuration class for managing application settings.
@@ -143,6 +363,17 @@ class Config:
         self.captionsFolder = "Captions"
         if not os.path.exists(self.captionsFolder):
             os.makedirs(self.captionsFolder)
+
+        self.saveFolder = "savedData"
+        self.fileTypes = ["topicModel", "topicsOverTime"]
+
+        for folder in self.fileTypes:
+            folderPath = os.path.join(self.saveFolder, folder)
+            if not os.path.exists(folderPath):
+                os.makedirs(folderPath)
+
+        self.representationModelType = "langchain"
+        self.useKeyBERT = True
 
     def set(self, name, value):
         """
@@ -231,3 +462,67 @@ class Config:
             sys.exit("Exiting due to configuration parameter import problems.")
         else:
             logging.info("All configuration parameters set up successfully.")
+
+
+class OpenAIBot:
+    """
+    A class representing an OpenAI Bot.
+
+    Attributes:
+        config (dict): The configuration parameters for the bot.
+        messages (list): A list to store the messages.
+        model (str): The model used by the bot.
+        client (AzureOpenAI): An instance of the AzureOpenAI client.
+
+    Methods:
+        __init__(self, config): Initializes a new instance of the OpenAIBot class.
+    """
+
+    def __init__(self, config):
+        self.config = config
+        self.messages = []
+
+        self.model = self.config.openAIParams["MODEL"]
+
+        self.client = AzureOpenAI(
+            api_key=self.config.openAIParams["KEY"],
+            api_version=self.config.openAIParams["VERSION"],
+            azure_endpoint=self.config.openAIParams["BASE"],
+            organization=self.config.openAIParams["ORGANIZATION"],
+        )
+
+
+class LangChainBot:
+    """
+    A class representing a language chain bot.
+
+    Attributes:
+        config (object): The configuration object for the bot.
+        messages (list): A list to store messages.
+        model (str): The model used by the bot.
+        client (object): The client object for the Azure OpenAI service.
+        chain (object): The QA chain object.
+
+    Methods:
+        __init__(self, config): Initializes the LangChainBot object.
+    """
+
+    def __init__(self, config):
+        self.config = config
+        self.messages = []
+
+        self.model = self.config.openAIParams["MODEL"]
+
+        self.client = langchainAzureOpenAI(
+            api_key=self.config.openAIParams["KEY"],
+            api_version=self.config.openAIParams["VERSION"],
+            azure_endpoint=self.config.openAIParams["BASE"],
+            organization=self.config.openAIParams["ORGANIZATION"],
+            azure_deployment=self.config.openAIParams["MODEL"],
+            temperature=0,
+        )
+
+        self.chain = load_qa_chain(
+            self.client,
+            chain_type="stuff",
+        )
